ce88f2bb972888578bcbf905d9c384042c2b0a66
Bird113 <barruetaalberto@yahoo.com>
2023-08-13 23:17:54 -0700

    created array for pokemonList

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList=[
+    {Name: 'Charmander', height: '0.6m', type: 'fire'},
+    {Name: 'Squirtle', height: '0.5m', type: 'water'},
+    {Name: 'Bulbasaur', height: '0.7m', type: 'grass'}  
+]
