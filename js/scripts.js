d8ae7c2ecb89f26a7a13e24569eee327a821d181
Bird113 <barruetaalberto@yahoo.com>
2023-08-15 16:11:58 -0700

    fixes the 'types' array format

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 let pokemonList=[
-    {Name: 'Charmander', height: '0.6m', type: 'fire'},
-    {Name: 'Squirtle', height: '0.5m', type: 'water'},
-    {Name: 'Bulbasaur', height: '0.7m', type: 'grass'}  
+  {Name: 'Charmander', 
+  height: '0.6m', 
+  type: ['fire']},
+
+  {Name: 'Squirtle', 
+  height: '0.5m', 
+  type: ['water']},
+
+  {Name: 'Bulbasaur', 
+  height: '0.7m', 
+  type: ['grass', 'poisin']}  
 ]
