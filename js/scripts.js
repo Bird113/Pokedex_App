d66c87c35d7c47c20f06d6f1072c48e788830d73
Bird113 <barruetaalberto@yahoo.com>
2023-08-16 00:48:52 -0700

    adds basic styling to app

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
 let pokemonList=[
   {Name: 'Charmander', 
-  height: '0.6m', 
+  height: 0.6, 
   type: ['fire']},
 
   {Name: 'Squirtle', 
-  height: '0.5m', 
+  height: 0.5, 
   type: ['water']},
 
   {Name: 'Bulbasaur', 
-  height: '0.7m', 
+  height: 0.7, 
   type: ['grass', 'poisin']}  
-]
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(`${pokemonList[i].Name} (height: ${pokemonList[i].height})`)
+  
+  if(pokemonList[i].height > 0.6)  {
+    document.write( 'Wow, that’s big!')
+  }
+ 
+}
