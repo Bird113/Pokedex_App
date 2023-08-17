c05d427b04883d255881a54194553e4f3d13a5f5
Bird113 <barruetaalberto@yahoo.com>
2023-08-16 18:58:33 -0700

    fixes loop & conditional

--- js/scripts.js
+++ js/scripts.js
@@ -11,12 +11,3 @@ let pokemonList=[
   height: 0.7, 
   type: ['grass', 'poisin']}  
 ]
-
-for (let i = 0; i < pokemonList.length; i++) {
-  document.write(`${pokemonList[i].Name} (height: ${pokemonList[i].height})`)
-  
-  if(pokemonList[i].height > 0.6)  {
-    document.write( 'Wow, that’s big!')
-  }
- 
-}
