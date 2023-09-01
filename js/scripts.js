eaf0d2ed8a90458a5fa7e2618df8195ee023f9d5
Bird113 <barruetaalberto@yahoo.com>
2023-08-31 17:04:28 -0700

    transition from 'for' to for each loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
 let pokemonList=[
-  {Name: 'Charmander', 
+  {name: 'Charmander', 
   height: 0.6, 
   type: ['fire']},
 
-  {Name: 'Squirtle', 
+  {name: 'Squirtle', 
   height: 0.5, 
   type: ['water']},
 
-  {Name: 'Bulbasaur', 
+  {name: 'Bulbasaur', 
   height: 0.7, 
   type: ['grass', 'poisin']}  
 ]
 
-for (let i = 0; i < pokemonList.length; i++) {
-  document.write(`${pokemonList[i].Name} (height: ${pokemonList[i].height})`)
-  
-  if(pokemonList[i].height > 0.6)  {
+  pokemonList.forEach(function(pokemon){
+    document.write(' ' + pokemon.name + pokemon.height + ' ');
+  });  
+   
+  if(pokemonList.height > 0.6)  {
     document.write( 'Wow, that’s big!')
   }
  
-}
