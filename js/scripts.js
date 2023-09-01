43a42cfe84f2175119c1f80c7030b544c21a8ea3
Bird113 <barruetaalberto@yahoo.com>
2023-08-31 17:39:46 -0700

    Added the IIFE
    added add and getAll keys

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,29 @@
-let pokemonList=[
-  {name: 'Charmander', 
-  height: 0.6, 
-  type: ['fire']},
+let pokemomRepository = (function () {
+  let pokemonList=[
+    {name: 'Charmander', 
+    height: 0.6, 
+    type: ['fire']},
 
-  {name: 'Squirtle', 
-  height: 0.5, 
-  type: ['water']},
+    {name: 'Squirtle', 
+    height: 0.5, 
+    type: ['water']},
 
-  {name: 'Bulbasaur', 
-  height: 0.7, 
-  type: ['grass', 'poisin']}  
-]
-// fixed for loop=> for each loop
-  pokemonList.forEach(function(pokemon){
-    document.write(' ' + pokemon.name + pokemon.height + ' ');
-  });  
-   
-  if(pokemonList.height > 0.6)  {
-    document.write( 'Wow, that’s big!')
+    {name: 'Bulbasaur', 
+    height: 0.7, 
+    type: ['grass', 'poisin']}  
+];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
- 
+
+  function getAll() {
+    return pokemonList
+  }
+
+  return{
+    add:add,
+    getAll:getAll
+  };
+})();
+
