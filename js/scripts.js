4f1e79d66a84e47dc37bf6a09939972184bc02de
Bird113 <barruetaalberto@yahoo.com>
2023-08-31 17:57:11 -0700

    added for each loop

--- js/scripts.js
+++ js/scripts.js
@@ -27,3 +27,12 @@ let pokemomRepository = (function () {
   };
 })();
 
+pokemomRepository.getAll().forEach(function (pokemon) {
+  let pokemonDetails = pokemon.name + " Height: " + pokemon.height ;
+  
+  // if(pokemon.height > 0.6) {
+  //   pokemonDetails + "THATS A BIG POKEMON!";
+  // }
+
+  document.write(pokemonDetails + "<br>");
+})
